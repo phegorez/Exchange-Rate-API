d96656a8f4f52bf7a456f45e4603271fb3d642ea
phegorez <metha.nompd@gmail.com>
2023-06-17 08:11:16 +0700

    s

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-const currency_one = document.getElementById('currency-one')
-const currency_two = document.getElementById('currency-two')
+//API https://v6.exchangerate-api.com/v6/0e6b664cbc953312f2a776bf/latest/
 
-const amount_one = document.getElementById('amount-one')
-const amount_two = document.getElementById('amount-two')
+//Include all elements
+const main_currency = document.querySelector('#main_currency')
+const exchange_currency = document.querySelector('#exchange_currency')
 
-const exchange_rate = document.getElementById('rate')
-const swap_btn = document.getElementById('btn-swap')
+const main_input = document.querySelector('#main_currency_value')
+const exchange_input = document.querySelector('#exchange_currency_value')
 
-const last_update = document.getElementById('update')
+const swapBtn = document.querySelector('#swap_curreny')
 
+const rate_text = document.querySelector('#rate')
 
-currency_one.addEventListener('change', calculateMoney)
-currency_two.addEventListener('change', calculateMoney)
+const APIUrl = 'https://v6.exchangerate-api.com/v6/0e6b664cbc953312f2a776bf/latest/'
 
-amount_one.addEventListener('input' , calculateMoney)
-amount_two.addEventListener('input', calculateMoney)
+main_currency.addEventListener('change', exchangeMoney)
+exchange_currency.addEventListener('change', exchangeMoney)
 
-function calculateMoney() {
-    const currency_one_value = currency_one.value
-    const currency_two_value = currency_two.value
+main_input.addEventListener('input', exchangeMoney)
+exchange_input.addEventListener('input', exchangeMoney)
 
-    let url = `https://v6.exchangerate-api.com/v6/96f63046eed6e6b4c696890a/latest/${currency_one_value}`
-    fetch(url).then(res => res.json()).then(data=> {
-        const rate = data.conversion_rates[currency_two_value]
-        const update_time = data.time_last_update_utc
-        exchange_rate.innerText = `1 ${currency_one_value} = ${rate} ${currency_two_value}`
-        amount_two.value = amount_one.value * rate
-        last_update.innerText = `Last Update is ${update_time}`
-    })
+function exchangeMoney () {
+    let main_value = main_currency.value
+    let exchange_value = exchange_currency.value
+
+    let API = APIUrl + main_value
+    fetch(API)
+        .then(res=>res.json())
+        .then(data=> {
+            let rate = data.conversion_rates[exchange_value]
+            rate_text.innerHTML = `1 ${main_value} = ${rate} ${exchange_value}`
+
+            exchange_input.value = (main_input.value * rate).toFixed(2)
+        })
 }
 
-swap_btn.addEventListener('click', () => {
-    const temp = currency_one.value
-    currency_one.value = currency_two.value
-    currency_two.value = temp
+swapBtn.addEventListener('click', () => {
+    const temp = main_currency.value
+    main_currency.value = exchange_currency.value
+    exchange_currency.value = temp
+
+    exchangeMoney()
 })
+
+exchangeMoney()
